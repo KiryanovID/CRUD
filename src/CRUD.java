03deb81e4e8446eaa28d6cbc46c57dd30f2f5198
KiryanovID <87255481+KiryanovID@users.noreply.github.com>
2022-02-18 11:30:10 +0400

    Не большие изменения
    Дополнен sql запрос в update.

--- src/CRUD.java
+++ src/CRUD.java
@@ -1,102 +1,63 @@
-import java.sql.Connection;
-import java.sql.ResultSet;
-import java.sql.SQLException;
-import java.sql.Statement;
-import java.util.ArrayList;
+import org.springframework.jdbc.core.JdbcTemplate;
+
 import java.util.List;
 import java.util.Locale;
 import java.util.Scanner;
 
 public class CRUD {
-    private final Connection conn = ConnectionDB.getInstance();
-    private Statement statement;
-    private ResultSet resultSet;
-    private int count = 0;
     private final Scanner scanner = new Scanner(System.in);
-
-    public CRUD() {
-        try {
-            statement = conn.createStatement();
-            resultSet = statement.executeQuery("Select max(id) as id from country;");
-            count = resultSet.getInt("id");
-        } catch (SQLException e) {
-            System.out.println("Что то пошло не так при инициализации класса CRUD");
-            e.printStackTrace();
-        }
-    }
+    private final MyJDBCTemplate myJDBCTemplate = new MyJDBCTemplate();
+    private final JdbcTemplate jdbcTemplate = myJDBCTemplate.getJdbcTemplate();
+    private static final String RED = "\u001B[31m";
+    private static final String RESET = "\033[0m";
+    private static final String GREEN = "\033[0;32m";
 
     public void create() {
-        try {
-            System.out.println("Введите название страны:");
-            String name = scanner.nextLine().trim();
-            System.out.println("Введите континет на котором находится страна:");
-            String conti = scanner.nextLine().toUpperCase(Locale.ROOT).trim();
-            Continent continent = Continent.valueOf(conti);
-            System.out.println("Введите площадь страны:");
-            long square = scanner.nextLong();
-            System.out.println("Введите население страны:");
-            int population = scanner.nextInt();
-            count++;
-            int id = count;
-            statement = conn.createStatement();
-            statement.execute("INSERT INTO 'country'('id', 'name', 'square', 'population', 'continent') " +
-                    "VALUES('" + id + "', '" + name + "','" + square + "','" + population + "','" + continent + "');");
-        } catch (SQLException e) {
-            e.printStackTrace();
-        }
-        System.out.println("Введите команду: ");
+        System.out.println("Введите название страны:");
+        String name = scanner.nextLine().trim();
+        System.out.println("Введите континет на котором находится страна:");
+        String conti = scanner.nextLine().toUpperCase(Locale.ROOT).trim();
+        System.out.println("Введите площадь страны:");
+        long square = scanner.nextLong();
+        System.out.println("Введите население страны:");
+        int population = scanner.nextInt();
+        Continent continent = Continent.valueOf(conti);
+        jdbcTemplate.update("INSERT INTO country(name,square,population,continent) VALUES(?,?,?,?)", name, square, population, continent);
+
+        System.out.println(GREEN + "Запись добавлена");
+        System.out.println(RESET + "Введите команду: ");
     }
 
     public void read() {
         System.out.println("введите ID страны");
         int find = scanner.nextInt();
-        try {
-            statement = conn.createStatement();
-            resultSet = statement.executeQuery("Select * from country where id = " + find + ";");
-            String name = resultSet.getString("name");
-            long square = resultSet.getLong("square");
-            int population = resultSet.getInt("population");
-            String continent = resultSet.getString("continent");
-            System.out.println("Страна " + name + "\n" +
-                    "площадь " + square + "кв.км\n" +
-                    "население " + population + "чел.\n" +
-                    "континет " + continent);
-        } catch (SQLException e) {
-            e.printStackTrace();
-        }
-        System.out.println("Введите команду: ");
 
+        Country country = jdbcTemplate.query("SELECT name, square, population, continent FROM country WHERE id = ?;",
+                new Object[]{find},
+                new CountryMapper()).stream().findAny().orElse(null);
+        System.out.println(country);
+        System.out.println("Введите команду: ");
     }
 
     public void update() {
-        Country tempCountry = null;
+
         System.out.println("введите ID страны");
         int find = scanner.nextInt();
         scanner.nextLine();
-        try {
-            statement = conn.createStatement();
-            resultSet = statement.executeQuery("Select * from country where id = " + find + ";");
-            String name = resultSet.getString("name");
-            long square = resultSet.getLong("square");
-            int population = resultSet.getInt("population");
-            String continent = resultSet.getString("continent");
-            Continent tempContinent = Continent.valueOf(continent);
-            tempCountry = new Country(name, square, population, tempContinent);
-        } catch (SQLException e) {
-            System.out.println("exception");
-        }
-
+        Country country = jdbcTemplate.query("SELECT name, square, population, continent FROM country WHERE id = ?;",
+                new Object[]{find},
+                new CountryMapper()).stream().findAny().orElse(null);
         System.out.println("Введите название страны:");
         String name = scanner.nextLine().trim();
         if (!name.equals("")) {
-            tempCountry.setName(name);
+            country.setName(name);
         }
         System.out.println("Введите континет на котором находится страна:");
         String conti = scanner.nextLine().toUpperCase(Locale.ROOT).trim();
         if (!conti.equals("")) {
             try {
                 Continent tempContinent = Continent.valueOf(conti);
-                tempCountry.setContinent(tempContinent);
+                country.setContinent(tempContinent);
             } catch (IllegalArgumentException e) {
                 System.out.println("нет такого континета, изменения не внесены");
             }
@@ -106,10 +67,10 @@ public class CRUD {
             String strLong = scanner.nextLine();
             long square = Long.parseLong(strLong);
             if (square < 0) {
-                tempCountry.setSquare(0);
+                country.setSquare(0);
                 System.out.println("Установленно значение по умолчанию равное 0");
             } else {
-                tempCountry.setSquare(square);
+                country.setSquare(square);
             }
         } catch (NumberFormatException e) {
             System.out.println("Значение не изменено");
@@ -120,62 +81,35 @@ public class CRUD {
             String strInt = scanner.nextLine();
             int population = Integer.parseInt(strInt);
             if (population < 0) {
-                tempCountry.setPopulation(0);
+                country.setPopulation(0);
                 System.out.println("Установленно значение по умолчанию равное 0");
             } else {
-                tempCountry.setPopulation(population);
+                country.setPopulation(population);
             }
         } catch (NumberFormatException e) {
             System.out.println("Значение не изменено");
         }
-        try {
-            statement = conn.createStatement();
-            statement.execute("UPDATE country " +
-                    "SET name = '" + tempCountry.getName() + "', " +
-                    "square = '" + tempCountry.getSquare() + "', " +
-                    "population = '" + tempCountry.getPopulation() + "', " +
-                    "continent = '" + tempCountry.getContinent() + "'" +
-                    "WHERE id = " + find + ";");
-        } catch (SQLException e) {
-            System.out.println("Ошибка записи в БД");
-        }
+        jdbcTemplate.update("UPDATE country SET name = ?, square = ?, population = ?, continent = ? WHERE id = ?;",
+                country.getName(), country.getSquare(), country.getPopulation(), country.getContinent(), find);
         System.out.println("Введите команду: ");
     }
 
     public void delete() {
+        String sql = "delete from country where id = ?;";
         System.out.println("введите ID страны");
         int find = scanner.nextInt();
-        try {
-            statement = conn.createStatement();
-            statement.execute("delete from country where id = " + find + ";");
-            System.out.println("Введите команду: ");
-
-        } catch (SQLException e) {
-            System.out.println("Что то пошло не так в методе delete");
-            e.printStackTrace();
-        }
+        jdbcTemplate.update(sql, find);
+        System.out.println(RED + "Запись удалена ");
+        System.out.println(RESET + "Введите команду: ");
     }
 
     public void showAll() {
-        List<Country> resultList = new ArrayList<>();
-        try {
-            statement = conn.createStatement();
-            resultSet = statement.executeQuery("Select * from country;");
-            while (resultSet.next()) {
-                String name = resultSet.getString("name");
-                long square = resultSet.getLong("square");
-                int population = resultSet.getInt("population");
-                String continent = resultSet.getString("continent");
-                Continent tempContinent = Continent.valueOf(continent);
-                resultList.add(new Country(name,square,population,tempContinent));
-            }
-            for (Country country : resultList){
-                System.out.println(country);
-            }
-            System.out.println("Введите команду: ");
-        } catch (SQLException e) {
-            System.out.println("что то пошло не так при выполнении метода showAllFromDB");
+        List<Country> country = jdbcTemplate.query("select * from country", new CountryMapper());
+        for (Country x : country) {
+            System.out.println(x);
         }
+        System.out.println("Введите команду: ");
+
     }
 
 
